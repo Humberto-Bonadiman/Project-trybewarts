a444787ae4a59dccdeddd42d7cfc7eaa0b520244
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-07-21 16:22:52 -0400

    EsLint

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 const loginButton = document.querySelector('#login-button');
 
-function checkLogin () {
-  let emailValue = email.value;
-  let passwordValue = password.value;
+function checkLogin() {
+  const emailValue = email.value;
+  const passwordValue = password.value;
   if (emailValue !== 'tryber@teste.com') {
     return alert('Login ou senha inválidos.');
-  } else if (passwordValue !== '123456') {
-    return alert('Login ou senha inválidos.');
-  } else {
-    return alert('Olá, Tryber!');
   }
+  if (passwordValue !== '123456') {
+    return alert('Login ou senha inválidos.');
+  }
+  return alert('Olá, Tryber!');
 }
 
 loginButton.addEventListener('click', checkLogin);
